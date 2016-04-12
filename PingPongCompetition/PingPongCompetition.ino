322034fede89eee0d2df8975a7c83efb6efae960
MarcianoPreciado <marcianocpreciado@gmail.com>
2016-04-12 17:36:10 -0600

    Fixed Several Functions

--- PingPongCompetition/PingPongCompetition.ino
+++ PingPongCompetition/PingPongCompetition.ino
@@ -18,7 +18,7 @@
   10 *~     Loader Servo        loaderServo     (Servo Instance)
   11 *~     Right Bumper        bumperR
   12        Left Bumper         bumperL
-  13
+  13        IR LED              LED
 
   A0
   A1
@@ -29,7 +29,7 @@
   A6
   A7        Built-In Buttons    btn
 
-*/
+**************************************************************************/
 
 
 #include <Servo.h>
@@ -53,38 +53,61 @@ Cannon Wallace(L1, L2, L3, L4, d1, d2, d3, thetaS0, thetaL0, v0);
 Servo cannonServo;
 Servo loaderServo;
 
-void setup() {
-  Serial.begin(9600);
-  cannonServo.attach(9);
-  loaderServo.attach(10);
-
-  pinMode(13, OUTPUT);
-  pinMode(MotorDirectionPin,OUTPUT);
-  pinMode(SolenoidDirectionPin,OUTPUT);
-  pinMode(bumperL,OUTPUT);
-  pinMode(bumperR,OUTPUT);
-}
+const int LED = 13;
 
 double targetX;
 double targetZ;
+byte junk;
 int angleLowerBound = 33;
 int angleUpperBound = 83;
-double thetaS;
+byte k = 0;
+
+double coordinates[6][2];
+double servoAngles[6];
+int pos;
+bool lastState;
+
+void setup() {
+  pinMode(MotorDirectionPin, OUTPUT);
+  pinMode(SolenoidDirectionPin, OUTPUT);
+  pinMode(bumperL, INPUT_PULLUP);
+  pinMode(bumperR, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+
+  Serial.begin(9600);               // initialize communication
+  Serial.write(97);                 // send 'a' confirmation value
+  double launchAngles[6];
+  int i;
+  for (i = 0; i < 6; i++) {
+    while (Serial.available() < 3); // waiting for coordinates to come through
+
+    launchAngles[i] = Wallace.getServoAngle(angleLowerBound, angleUpperBound, coordinates[i][0]);
+    servoAngles[i] = Wallace.servoAngle(launchAngles[i]);
+  }
+  
+  cannonServo.attach(9);            // attach servos
+  loaderServo.attach(10);
+  Wallace.returnHome(lastState, pos);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+}
 
 void loop() {
-  digitalWrite(13, HIGH);
-  if ( //we are given a target ){
-    thetaS = getServoAngle(angleLowerBound, angleUpperBound, targetX);
-    cannonServo.write(thetaS);
-
-    Wallace.moveTo(targetZ);
-
+int j;
+  for (j = 0; j < 6; j++) {
+    Wallace.moveTo(coordinates[j][1], lastState, pos);
+    cannonServo.write(servoAngles[j]);
+    delay(350);
     analogWrite(SolenoidPowerPin, fullPower);
     delay(onTime);
     analogWrite(SolenoidPowerPin, 0);
+    Wallace.reload(cannonServo, loaderServo,lastState, pos);
+  }
 
-    Wallace.reload();
-}
+  Wallace.returnHome(lastState, pos);           // finish game
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
 
-delay(10);
 }
